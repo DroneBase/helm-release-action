1591b901a5f415ba61f21773ba6c296deb03dcea
Branton Davis <branton.davis@dronebase.com>
2024-07-22 17:27:32 -0500

    Remove repo add.

--- index.js
+++ index.js
@@ -7,20 +7,6 @@ const HELM = 'helm';
 const REPO_ALIAS = 'repo';
 const RELEASE_DIR = '.release/';
 
-// Returns argument required to register the S3 repository.
-function repo() {
-  const repo = core.getInput('repo', {
-    required: true,
-  });
-
-  return [
-    'repo',
-    'add',
-    REPO_ALIAS,
-    repo,
-  ];
-}
-
 // Returns argument required to generate the chart package.
 function package() {
   const args = [
@@ -45,7 +31,7 @@ function package() {
   return args;
 }
 
-// Returns argument required to push the chart release to S3 repository.
+// Returns argument required to push the chart release to OCI repository.
 function push() {
   const releaseFile = path.join(RELEASE_DIR, fs.readdirSync(RELEASE_DIR)[0]);
   const args = [
@@ -66,7 +52,6 @@ function push() {
 
 async function main() {
   try {
-    await exec.exec(HELM, repo()),
     await exec.exec(HELM, package()),
     await exec.exec(HELM, push())
   } catch (err) {
